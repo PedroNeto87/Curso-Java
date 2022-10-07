4ad7960edf3b9325e3a367638e09c625bde69c51
Pedro Neto <pedrofcn01@hotmail.com>
2022-10-07 16:51:46 -0300

    Seção 15 - Tratamento de exceções - try/catch - pilha de chamadas de metodos - bloco finally

---
+++ aula_172BlocoFinally/src/aula_172BlocoFinally/Program.java
@@ -0,0 +1,29 @@
+package aula_172BlocoFinally;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		File file = new File("C:\\Users\\pedro\\Desktop\\comprar.txt");
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch(IOException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		}
+		finally {
+			if(sc != null) {
+				sc.close();
+			}
+		}		
+	}
+}
